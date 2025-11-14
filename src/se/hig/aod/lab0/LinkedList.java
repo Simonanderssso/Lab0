d280c5ae343c16ac1e0f2e0f51aa74124b65514e
Simon Andersson <simon.andersson22@outlook.com>
2025-11-14 18:54:37 +0100

    Initial commit

---
+++ src/se/hig/aod/lab0/LinkedList.java
@@ -0,0 +1,48 @@
+package se.hig.aod.lab0;
+
+public class LinkedList<T> implements List<T> {
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int numberOfElements() {
+        return 0;
+    }
+
+    @Override
+    public void insertFirst(T t) {
+
+    }
+
+    @Override
+    public void insertLast(T t) {
+
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T getFirst() {
+        return null;
+    }
+
+    @Override
+    public T getLast() {
+        return null;
+    }
+}
